ea52ccf2a0997dd313cf8089f3f236b4ad6d5bf6
Stanislav Mukhin <svisor84@mail.ru>
2016-08-13 21:32:20 +0300

    рабочая версия

--- vk.js
+++ vk.js
@@ -8,6 +8,7 @@ new Promise(function(resolve) {
         window.onload = resolve;
     }
 }).then(function() {
+        //запрашиваем авторизацию
         return new Promise(function(resolve, reject) {
             VK.init({
                 apiId: 5568346
@@ -21,6 +22,7 @@ new Promise(function(resolve) {
             }, 2);
         });
     }).then(function() {
+//Возвращаем Имя Фамилию проскланенную
         return new Promise(function(resolve, reject) {
             VK.api('users.get', {'name_case': 'gen'}, response => {
                     if (response.error) {
@@ -34,24 +36,84 @@ new Promise(function(resolve) {
         })
     }).then(function() {
         return new Promise(function(resolve, reject) {
-            VK.api('friends.get', {'order':'random','fields':'nickname,bdate,city,photo_50'},
+            VK.api('friends.get', {'fields':'bdate,photo_50'},
                 function(response) {
                     if (response.error) {
                         reject(new Error(response.error.error_msg));
                     } else {
-                        function mySort(a, b) {
-                            if(a.bdate && b.bdate){
-                                var [bDateF, bMonthF,] = a.bdate.split('.'),
-                                    [bDateS, bMonthS,] = b.bdate.split('.');
-                                var curDate = new Date();
-                                if (bDateF > bDateS) return 1;
-                                if (bDateF < bDateS) return -1;
+
+                        var myFriends = [];
+                        var curDate = new Date();
+
+//делаю корректную дату
+                        var trueDate = {
+                            day: curDate.getDate(),
+                            month: curDate.getMonth(),
+                            year: curDate.getFullYear()
+                        };
+//вычисляем возраст
+                        function yearBDay(bdate) {
+                            if(!bdate) return "";
+
+                            var [,,year] = bdate.split('.');
+                            if(!year)
+                                return "";
+                            else
+                                return trueDate.year - year;
+                        }
+
+// перебираем пришедшие данные
+                        for(var item = 0; item < response.response.length; item++){
+                            myFriends[myFriends.length] = {
+                                first_name : response.response[item].first_name,
+                                last_name  : response.response[item].last_name,
+                                photo_50  : response.response[item].photo_50,
+                                bdate      : response.response[item].bdate,
+                                year       : yearBDay(response.response[item].bdate)
                             }
                         }
-                        var res = response.response.sort(mySort);
+
+                        var noBday = [],
+                            afterBday = [],
+                            beforeBday = [];
+
+//сортируем людей, у которых указан ДР
+                        var friendWithBday = myFriends.filter(function(elem){
+                            if(elem.bdate !== undefined){
+                                return elem;
+                            }else{
+                                noBday[noBday.length] = elem;
+                            }
+                        }).sort(function(a,b){
+                            var [aDay,aMonth,] = a.bdate.split('.');
+                            var [bDay, bMonth,] = b.bdate.split('.');
+
+                             if(+aMonth > +bMonth) return 1;
+                             else if(+aMonth < +bMonth) return -1;
+
+                             if(+aDay >= +bDay) return 1;
+                             else if(+aDay < +bDay) return -1;
+
+                            /*return (+aMonth > +bMonth) ?  1 : ((+aMonth < +bMonth) ? -1 : ''),
+                                (+aDay >= +bDay) ?  1 : ((+aDay < +bDay) ? -1 : '');*/
+
+
+
+                        });
+
+// сортируем людей , в зависимости от текущей даты по дате рождения
+                        friendWithBday.map(function(elem){
+                            var [aDay, aMonth] = elem.bdate.split('.');
+                            if(aMonth > trueDate.month) return afterBday[afterBday.length] = elem;
+                            else if(aMonth < trueDate.month) return beforeBday[beforeBday.length] = elem;
+
+                            if(aDay >= trueDate.day) return afterBday[afterBday.length] = elem;
+                            else if(aDay < trueDate.day) return beforeBday[beforeBday.length] = elem;
+
+                        });
                         var source = friendsItemTemplate.innerHTML;
                         var templateFn = Handlebars.compile(source);
-                        var template = templateFn({list: res});
+                        var template = templateFn({list: [].concat(afterBday,beforeBday, noBday)});
                         result.innerHTML = template;
                         resolve();
                     }
@@ -60,3 +122,4 @@ new Promise(function(resolve) {
     }).catch(function(e) {
         alert('Ошибка: ' + e.message);
     });
+// убить авторизацию в браузере VK.Auth.revokeGrants();
