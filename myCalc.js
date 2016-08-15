c5f49cdeb03f1f6d8660c497bf95b0d54bdc27a3
Stanislav Mukhin <svisor84@mail.ru>
2016-08-16 00:08:48 +0300

    есть два решения, но не решена проблема передачи аргумента на метод родителя ))))

--- myCalc.js
+++ myCalc.js
@@ -1,80 +1,76 @@
-//function Calculator(firstNumber) {
-//    this.initial = firstNumber;
-//}
-//
-//Calculator.prototype.sum = function () {
-//    var res = 0;
-//    for (var i = 0; i < arguments.length; i++) {
-//        res += arguments[i];
-//    }
-//    return res + this.initial;
-//};
-//
-//Calculator.prototype.dif = function () {
-//    var res = 0;
-//    for (var i = 0; i < arguments.length; i++) {
-//        res += arguments[i];
-//    }
-//    return this.initial - res;
-//};
-//
-//Calculator.prototype.div = function () {
-//    try {
-//        var res = 0;
-//        for (var i = 0; i < arguments.length; i++) {
-//            if (arguments[i] == 0) {
-//                throw new arguments.error
-//            }
-//            res += arguments[i];
-//        }
-//        return this.initial / res;
-//    }
-//    catch (e) {
-//        console.log('Ошибка ' + e.name + ": на 0 делить нельзя");
-//    }
-//};
-//
-//Calculator.prototype.mul = function () {
-//    var res = 0;
-//    for (var i = 0; i < arguments.length; i++) {
-//        res += arguments[i];
-//    }
-//    return this.initial * res;
-//};
-//
-//function SqlCalc(value){
-//    Calculator.call(this, value);
-//    this.initial = value;
-//}
-//
-//SqlCalc.prototype = Object.create(Calculator.prototype);
-//
-//
-//
-//SqlCalc.prototype.sum = function () {
-//   var arg = [].slice.call(arguments);
-//    console.log(arg)
-//    var res = Calculator.prototype.sum.call(this);
-//    return res*res
-//};
-///*SqlCalc.prototype.dif = function (){
-//    Calculator.prototype.dif.call(this);
-//};
-//SqlCalc.prototype.div = function (){
-//    Calculator.prototype.div.call(this);
-//};
-//SqlCalc.prototype.mul = function (){
-//    Calculator.prototype.mul.call(this);
-//};*/
-//
-//var myCalculator = new SqlCalc(100);
-//
-//
-//console.log(myCalculator.sum(1, 2, 3)); //вернет 11 236 (100 + 1 + 2 + 3 = 106 * 106)
-//console.log(myCalculator.dif(10, 20)); //вернет 4 900
-//console.log(myCalculator.div(2, 2)); //вернет 625
-//console.log(myCalculator.mul(2, 2)); //вернет 160 000
-'use strict';
+function Calculator(firstNumber) {
+    this.initial = firstNumber;
+}
+
+Calculator.prototype.sum = function () {
+    var res = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        res += arguments[i];
+    }
+    return res + this.initial;
+};
+
+Calculator.prototype.dif = function () {
+    var res = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        res += arguments[i];
+    }
+    return this.initial - res;
+};
+
+Calculator.prototype.div = function () {
+    try {
+        var res = 0;
+        for (var i = 0; i < arguments.length; i++) {
+            if (arguments[i] == 0) {
+                throw new arguments.error
+            }
+            res += arguments[i];
+        }
+        return this.initial / res;
+    }
+    catch (e) {
+        console.log('Ошибка ' + e.name + ": на 0 делить нельзя");
+    }
+};
+
+Calculator.prototype.mul = function () {
+    var res = 0;
+    for (var i = 0; i < arguments.length; i++) {
+        res += arguments[i];
+    }
+    return this.initial * res;
+};
+
+function SqlCalc(value){
+    Calculator.call(this, value);
+    this.initial = value;
+}
+
+SqlCalc.prototype = Object.create(Calculator.prototype);
+
+SqlCalc.prototype.sum = function () {
+  // var arg = [].slice.call(arguments);
+    var res = Calculator.prototype.sum.call(this);
+    return res*res
+};
+SqlCalc.prototype.dif = function (){
+   var res = Calculator.prototype.dif.call(this);
+    return res * res;
+};
+SqlCalc.prototype.div = function (){
+    var res = Calculator.prototype.div.call(this);
+    return res * res;
+};
+SqlCalc.prototype.mul = function (){
+    var res = Calculator.prototype.mul.call(this);
+    return res * res;
+};
+
+/////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+// Решение ES6
+
+/*'use strict';
 
 class Calculator {
     constructor(initial) {
@@ -127,12 +123,12 @@ class SqlCalc extends Calculator{
         super(initial);
         this.initial = initial;
     }
-    sum(){
+sum(){
         var res = super.sum();
        return res * res;
     }
-    def(){
-        var res = super.def();
+    dif(){
+        var res = super.dif();
         return res * res;
     }
     div(){
@@ -143,12 +139,13 @@ class SqlCalc extends Calculator{
         var res = super.mul();
         return res * res;
     }
-}
+
+}*/
 
 
 
-let myCalculator = new SqlCalc(100);
+var myCalculator = new SqlCalc(100);
 console.log(myCalculator.sum(1, 2, 3)); //вернет 11 236 (100 + 1 + 2 + 3 = 106 * 106)
-//console.log(myCalculator.dif(10, 20)); //вернет 4 900
-//console.log(myCalculator.div(2, 2)); //вернет 625
-//console.log(myCalculator.mul(2, 2)); //вернет 160 000
+console.log(myCalculator.dif(10, 20)); //вернет 4 900
+console.log(myCalculator.div(2, 2)); //вернет 625
+console.log(myCalculator.mul(2, 2)); //вернет 160 000
