3b2d922e56d5ea4be829bb9491e9c04ec05f4fee
Stanislav Mukhin <svisor84@mail.ru>
2016-08-17 23:49:55 +0300

    init

---
+++ friendfilter/ff3.js
@@ -0,0 +1,47 @@
+// Делаем D&D
+
+var myFriend = document.querySelector('.friendsResult');
+
+var activeElement;
+
+var offsetX = 0;
+
+var offsetY = 0;
+
+
+var mDown = (e) => {
+
+    if(e.target.classList.contains('myFriend')) {
+
+        activeElement = e.target;
+
+        offsetX = e.offsetX;
+
+        offsetY = e.offsetY;
+
+        document.addEventListener('mouseup', mUp);
+
+        document.addEventListener('mousemove', mMove);
+    }
+};
+
+var mUp = (e) => {
+
+    activeElement = null;
+
+    document.removeEventListener('mouseup', mUp);
+
+    document.removeEventListener('mousemove', mMove);
+};
+
+var mMove = (e) => {
+
+    if (activeElement) {
+
+        activeElement.style.top = (e.clientY - offsetY) + 'px';
+
+        activeElement.style.left = (e.clientX - offsetX) + 'px';
+    }
+};
+
+document.addEventListener('mousedown', mDown);
