f8bdeeba555ac99b83df47a24c7dea31856d126a
Stanislav Mukhin <svisor84@mail.ru>
2016-09-05 00:02:13 +0300

    готово

---
+++ nodejs/nodejs.js
@@ -0,0 +1,24 @@
+/**
+ * Created by Stas on 03.09.2016.
+ */
+var fs = require('fs');
+var startDir = './nodejs';
+var scan = function(startDir){
+    var dirs = fs.readdirSync(startDir);
+
+    console.log(dirs);
+
+    for(var dir of dirs){
+    dir = startDir + '/' +dir;
+        var stat = fs.statSync(dir);
+        if(!stat.isFile()){
+            scan(dir);
+        }else{
+            console.log(dir,(stat.size/1024).toFixed(3) + ' kb');
+        }
+
+    }
+};
+scan(startDir);
+
+
